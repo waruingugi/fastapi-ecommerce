56a23ea2176854d9568ebfe7e9e0dce153f98569
waruingugi <waruingugientp@gmail.com>
2023-01-23 21:51:05 +0300

    Created schemas

---
+++ app/errors/custom.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class ErrorCodes(str, Enum):
+    INVALID_PHONENUMBER = "Phone number is not valid"
---
+++ app/users/serailizers/user.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, EmailStr, validator
+from app.users.constants import UserTypes
+from enum import Enum
+from app.core.helpers import capitalize_fields, validate_phone_number
+from datetime import datetime
+
+class UserBaseSerializer(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    phone: str
+    email: EmailStr
+    is_verified: bool | None = False
+
+
+class UserCreateSerializer(UserBaseSerializer):
+    user_type: str = UserTypes.CUSTOMER.value
+    password: str | None = None
+
+    _capitalize_fields = validator("first_name", "last_name", pre=True, allow_reuse=True)(
+        capitalize_fields
+    )
+
+    _validate_phone_number = validator("phone", pre=True, allow_reuse=True)(
+        validate_phone_number
+    )
+
+
+class UserActivateSerializer(UserCreateSerializer):
+    is_active: bool = True
+
+
+class UserUpdateSerializer(UserBaseSerializer):
+    password: str | None = None
+
+
+class UserInDBSerializer(UserBaseSerializer):
+    id: str
+    password: str
+    user_type: str
