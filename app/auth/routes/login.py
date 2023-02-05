95a9281485d77aeaac3d586f47cb2f75851e22ea
waruingugi <waruingugientp@gmail.com>
2023-02-05 22:17:37 +0300

    Create login route

--- app/auth/routes/login.py
+++ app/auth/routes/login.py
@@ -1,4 +1,25 @@
 from fastapi import APIRouter
-
+from fastapi.security import OAuth2PasswordRequestForm
+from app.core.deps import get_db
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.core.security import authenticate_user, create_access_token
 
 router = APIRouter()
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    db: Session = Depends(get_db),
+    form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect phone or password")
+
+    access_token = create_access_token(
+        data={"sub": user.phone, "scopes": form_data.scopes},
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/core/deps.py
+++ app/core/deps.py
@@ -10,6 +10,8 @@ from jose import JWTError, jwt
 from pydantic import BaseModel, ValidationError
 from app.users.daos.user import user_dao
 from app.users.serializers.user import UserBaseSerializer
+from sqlalchemy.orm import Session
+from app.core.config import get_app_settings
 
 
 class TokenData(BaseModel):
@@ -39,6 +41,7 @@ async def get_current_user(
     security_scopes: SecurityScopes,
     token: str = Depends(oauth2_scheme)
 ):
+    settings = get_app_settings()
     if security_scopes.scopes:
         authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
     else:
@@ -76,6 +79,7 @@ async def get_current_user(
             )
 
     return user
+
 
 async def get_current_active_user(
     current_user: UserBaseSerializer = Security(get_current_user, scopes=["me"])
