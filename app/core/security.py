0e3538fdd3aa7d8b0128e010065c27c44d29826f
waruingugi <waruingugientp@gmail.com>
2023-02-17 07:21:34 +0300

    Modify create access token func

--- app/core/security.py
+++ app/core/security.py
@@ -10,9 +10,7 @@ from fastapi import Depends, FastAPI, HTTPException, Security, status
 from app.core.config import get_app_settings
 # from app.auth.serializers.auth import TokenData
 from jose import JWTError, jwt
-from pydantic import ValidationError
 from sqlalchemy.orm import Session
-from app.users.serializers.user import UserBaseSerializer
 
 
 settings = get_app_settings()
@@ -28,17 +26,72 @@ def get_password_hash(password: str):
     return pwd_context.hash(password)
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
+def create_access_token(
+    db: Session, subject: str, grant_type: str, expires_in: int | None
+) -> dict:
+    expires_in_seconds = (
+        expires_in if expires_in else settings.ACCESS_TOKEN_EXPIRY_IN_SECONDS
+    )
+    refresh_expires_in_seconds = settings.REFRESH_TOKEN_EXPIRY_IN_SECONDS
 
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(
-            seconds=settings.ACCESS_TOKEN_EXPIRY_IN_SECONDS
-        )
+    to_encode = {
+        "iat": int(datetime.utcnow().timestamp()),
+        "exp": datetime.utcnow() + timedelta(seconds=expires_in_seconds),
+        "user_id": str(subject),
+        "grant_type": grant_type
+    }
 
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    # Create access token
+    token = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    # Create refresh token
+    to_encode["exp"] = datetime.utcnow() + timedelta(seconds=refresh_expires_in_seconds)
+    to_encode["iat"] = int(datetime.utcnow().timestamp())
 
-    return encoded_jwt
+    refresh_token = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+    token_data = {
+        "access_token": token,
+        "refresh_token": refresh_token,
+        "token_type": grant_type,
+        "expires_in": expires_in_seconds,
+        "refresh_expires_in": refresh_expires_in_seconds,
+        "user_id": subject,
+    }
+    return token_data
+
+
+# def create_access_token(data: dict, expires_delta: timedelta | None = None):
+#     to_encode = data.copy()
+
+#     if expires_delta:
+#         expire = datetime.utcnow() + expires_delta
+#     else:
+#         expire = datetime.utcnow() + timedelta(
+#             seconds=settings.ACCESS_TOKEN_EXPIRY_IN_SECONDS
+#         )
+    
+#     refresh_expires_in_seconds = settings.REFRESH_TOKEN_EXPIRY_IN_SECONDS
+
+#     to_encode.update({
+#         "iat": int(datetime.utcnow().timestamp()),
+#         "exp": expire
+#     })
+#     token = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+#     # Create refresh token also
+#     to_encode["exp"] = datetime.utcnow() + timedelta(
+#         seconds=refresh_expires_in_seconds
+#     )
+#     to_encode["iat"] = int(datetime.utcnow().timestamp())
+
+#     refresh_token = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+#     token_data = {
+#         "access_token": token,
+#         "refresh_token": refresh_token,
+#         "token_type": data["grant_type"],
+#         "expires_in": settings.ACCESS_TOKEN_EXPIRY_IN_SECONDS,
+#         "refresh_token_ein": refresh_expires_in_seconds,
+#         "sub": data["sub"],
+#     }
+
+#     return token_data
