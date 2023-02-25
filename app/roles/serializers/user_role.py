0d398cf83d6ce2327f6b8b043a65d2db2c16bada
waruingugi <waruingugientp@gmail.com>
2023-02-25 13:41:48 +0300

    Create scope validator

--- app/roles/serializers/user_role.py
+++ app/roles/serializers/user_role.py
@@ -1,12 +1,22 @@
 from app.db.serializer import InDBBaseSerializer
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import List
 from app.roles.constants import UserScopeTypes
+from app.exceptions.custom import InvalidUserScopeType
 
 
 class UserRoleBaseSerializer(BaseModel):
     name: str
-    scope: UserScopeTypes | None
+    scope: str | None
+
+    @validator('scope', pre=True)
+    def is_valid_scope(cls, value):
+        """Validate scoper is a valid UserScopeTypes object"""
+        for scope in UserScopeTypes:
+            if scope.value == value:
+                return value
+
+        return InvalidUserScopeType(f"{value} scope does not exist")
 
 
 class UserRoleInDBSerializer(InDBBaseSerializer, UserRoleBaseSerializer):
