e7fa049f6456d4e1e0850ffc61749e3af466eff4
waruingugi <waruingugientp@gmail.com>
2023-03-04 12:13:10 +0300

    Add UserFilter

--- app/users/filters.py
+++ app/users/filters.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
 from app.users.constants import UserTypes
+from fastapi_sqlalchemy_filter import Filter
+from app.users.models import User
 
 
-class UserFilter(BaseModel):
+class UserFilter(Filter):
+    contact: str | None
     is_active: bool | None
-    user_types: UserTypes | None
+    user_type: UserTypes | None
+
+    class Constants(Filter.Constants):
+        model = User
+        search_field_name = "contact"
+        search_model_fields = ["phone", "email"]
