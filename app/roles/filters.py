2bcbf81c1a3d02642fd3d8a9e669eb7ab5cdab0d
waruingugi <waruingugientp@gmail.com>
2023-03-09 16:32:44 +0300

    Improve UserRole Filter

--- app/roles/filters.py
+++ app/roles/filters.py
@@ -1,17 +1,17 @@
-from fastapi_sqlalchemy_filter import Filter
+from fastapi_sqlalchemy_filter import Filter, FilterDepends, with_prefix
 from app.roles.models import UserRole, Role
-
-
-class UserRoleFilter(Filter):
-    name: str | None
-    user_id: str | None
-
-    class Constants(Filter.Constants):
-        model = UserRole
+from app.users.constants import UserTypes
 
 
 class RoleFilter(Filter):
-    name: str | None
+    name: UserTypes | None
 
     class Constants(Filter.Constants):
         model = Role
+
+
+class UserRoleFilter(Filter):
+    role: RoleFilter | None = FilterDepends(with_prefix("role", RoleFilter))
+
+    class Constants(Filter.Constants):
+        model = UserRole
