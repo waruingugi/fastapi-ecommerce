8ab761fd5b0861db4b69c9cd243f0a9587a43a58
waruingugi <waruingugientp@gmail.com>
2023-03-10 08:25:58 +0300

    Migrate to mapped_column

--- app/users/models.py
+++ app/users/models.py
@@ -1,18 +1,18 @@
-from sqlalchemy import Column, String, Boolean, DateTime
-from sqlalchemy.orm import Mapped
+from sqlalchemy import String, Boolean, DateTime
+from sqlalchemy.orm import mapped_column
 from app.db.base_class import Base, get_current_datetime
 from app.users.constants import UserTypes
 
 
 class User(Base):
-    date_joined = Column(DateTime, default=get_current_datetime)
-    first_name: str = Column(String, nullable=True)
-    last_name: str = Column(String, nullable=True)
-    phone: Mapped[str] = Column(String, nullable=False, index=True)
-    email: str = Column(String, unique=True, index=True, nullable=True)
-    is_active = Column(Boolean, default=False)
-    user_type = Column(String, default=UserTypes.CUSTOMER.value)
-    hashed_password: str = Column(String, nullable=False)
+    date_joined = mapped_column(DateTime, default=get_current_datetime)
+    first_name = mapped_column(String, nullable=True)
+    last_name = mapped_column(String, nullable=True)
+    phone = mapped_column(String, nullable=False, index=True)
+    email = mapped_column(String, unique=True, index=True, nullable=True)
+    is_active = mapped_column(Boolean, default=False)
+    user_type = mapped_column(String, default=UserTypes.CUSTOMER.value)
+    hashed_password = mapped_column(String, nullable=False)
 
     @property
     def get_username(self) -> str:
